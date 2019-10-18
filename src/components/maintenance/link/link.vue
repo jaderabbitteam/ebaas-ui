094273e7dbd5afe1db2779a681c94dd3693df6dc
zhaosuizhen <1026688629@qq.com>
2019-10-18 11:06:05 +0800

    Update link.vue

---
+++ src/components/maintenance/link/link.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      业务链管理
+  </div>
+</template>
+
+<script>
+export default {
+    name:"link",
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
