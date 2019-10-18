8627ffe83174251a23da35e7763addf99beb98e6
zhaosuizhen <1026688629@qq.com>
2019-10-18 11:10:54 +0800

    Update user_manage.vue

---
+++ src/components/maintenance/user_manage/user_manage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      用户信息页面
+  </div>
+</template>
+
+<script>
+export default {
+    name:"user_manage",
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
