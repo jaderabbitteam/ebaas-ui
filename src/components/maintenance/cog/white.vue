9decd0070d6fede711a6752b7c02fe9be186e456
zhaosuizhen <1026688629@qq.com>
2019-10-18 11:25:50 +0800

    Update white.vue

---
+++ src/components/maintenance/cog/white.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      白名单
+  </div>
+</template>
+
+<script>
+export default {
+    name:"white",
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
