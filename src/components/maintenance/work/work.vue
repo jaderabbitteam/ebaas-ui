7b226fc1dac2a4191bd5fb67fb00b973ddbcb1f5
zhaosuizhen <1026688629@qq.com>
2019-10-18 11:13:12 +0800

    Update work.vue

---
+++ src/components/maintenance/work/work.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      业务管理
+  </div>
+</template>
+
+<script>
+export default {
+    name:"work",
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
