cad7905512f7f0b1f6f04cbecdd9caa376ab260e
zhaosuizhen <1026688629@qq.com>
2019-10-18 11:03:50 +0800

    Update home.vue

---
+++ src/components/maintenance/home/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      这里是主页
+  </div>
+</template>
+
+<script>
+export default {
+    name:"home",
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scode>
+
+</style>
