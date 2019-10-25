32ceecdca21e7592c943bd57a614432e6005dd48
zhousk <846838816@qq.com>
2019-10-25 10:14:53 +0800

    Update app.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,13 @@
 import { http } from '@/utils'
 
-const baseUrl = 'http://192.168.34.134:8080/v1'
+const baseUrl = 'http://183.6.121.188:19281/v1'
 
 
 // 审核状态
-export const user = () => http.get(baseUrl + 'user/login/rabbit-root/root', {
+export const user = () => http.get(baseUrl + '/user/?userName=yanshi-zhangsan&password=123', {
     params: {}
 }).then(data => {
     console.log(data);
-    
+
     return data
 })
